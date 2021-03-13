3cbc9afd3d2cecacd6d07e500184c90618f41f4e
kimamovic21 <79793398+kimamovic21@users.noreply.github.com>
2021-03-13 09:19:58 +0100

    Add files via upload

---
+++ ex06/src/index.js
@@ -0,0 +1,7 @@
+function main() {
+    var myName = "Kerim";
+    var myString = "My name is " + myName + " and I am well!";
+    return myString;   
+  }
+  console.log(main());
+  module.exports = main;
