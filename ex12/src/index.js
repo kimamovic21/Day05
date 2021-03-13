b73eee134b1ab182791b19319362c304d1ad45c0
kimamovic21 <79793398+kimamovic21@users.noreply.github.com>
2021-03-13 09:21:30 +0100

    Add files via upload

---
+++ ex12/src/index.js
@@ -0,0 +1,7 @@
+function main() {
+    var lastName = "Tesla";
+    var lastLetterOfLastName = firstName[firstName.length - 1];
+    return lastLetterOfLastName;
+  }
+  console.log(main());
+  module.exports = main;
